213c812999a8310a1f8a46a94682f644eca9cdc5
Larson007 <mohamed.benallal.pro@gmail.com>
2022-02-25 16:23:14 +0100

    Js - Module 05 - TodoList - 25/02/22

--- Modules/02-Developpement/Javascript/Module 05 - VueJs/Projets/ToDoListe/Exercice/assets/js/app.js
+++ Modules/02-Developpement/Javascript/Module 05 - VueJs/Projets/ToDoListe/Exercice/assets/js/app.js
@@ -4,13 +4,13 @@ const app = Vue.createApp({
             title: 'TodoListe',
             taskText: '',
             allTasks: [],
-
         };
     },
     methods: {
         addTask(e) {
             e.preventDefault();
             this.allTasks.push({ id: Date.now(), name: this.taskText, check: false });
+            //localStorage.setItem("tasks", JSON.stringify(this.allTasks));
             this.taskText = "";
         },
         checkTask(e) {
@@ -23,8 +23,23 @@ const app = Vue.createApp({
             this.allTasks = this.allTasks.filter(
                 x => Number(x.id) !== Number(e.target.dataset.key)
             );
+            //localStorage.setItem("tasks", JSON.stringify(this.allTasks));
         }
     },
+    mounted() {
+        // Tâche executé à chaque foisnque l'application Vue est lancé
+        this.allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    },
+    watch: {
+        // Compare la nouvelle valeur a l'ancienne à chaque execution
+        allTasks: {
+            // Permet de vérifier que les donnée d'un element constant comme un array évolue
+            deep: true,
+            handler(newValue, oldValue) {
+                localStorage.setItem("tasks", JSON.stringify(newValue));
+            }
+        }
+    }
 });
 
 app.mount('#app');
