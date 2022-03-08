a24a6e54d9c1e232a36f0f10dfdcaf22d015ebc0
Larson007 <mohamed.benallal.pro@gmail.com>
2022-03-08 23:15:52 +0100

    PHP - Module 02 - MVC Page user - 08/03/22

--- Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/03 - MVC/Controllers/ContactController.php
+++ Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/03 - MVC/Controllers/ContactController.php
@@ -4,5 +4,6 @@ require_once dirname(__DIR__) . '/models/Users.php';
 
 $user = findUserById($_GET['id']);
 
-var_dump($user);
+//var_dump($user);
+
 require_once dirname(__DIR__) . '/views/contact.phtml';
