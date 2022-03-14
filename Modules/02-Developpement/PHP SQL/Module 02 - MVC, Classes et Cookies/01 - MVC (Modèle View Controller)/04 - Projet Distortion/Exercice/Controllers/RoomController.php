e1d96e8e50acf9e0f8754ede23a5797969418e2b
Larson007 <mohamed.benallal.pro@gmail.com>
2022-03-14 14:17:56 +0100

    PHP Distortion - Messages Pinned - 14/03/22

--- Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/04 - Projet Distortion/Exercice/Controllers/RoomController.php
+++ Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/04 - Projet Distortion/Exercice/Controllers/RoomController.php
@@ -15,4 +15,20 @@ if(isset($_POST['addMessage']) && !empty($_POST['addMessage'])) {
 $messages = showMessages($_GET['room']);
 
 
+$url = basename($_SERVER['REQUEST_URI']);    
+
+// Pinned
+if(isset($_POST['addPin']) && !empty($_POST['addPin'])) {
+    $pinned =  addPinnedMessage($_POST['addPin']);
+    header('Location: '. $url);
+} else if (isset($_POST['removePin']) && !empty($_POST['removePin'])) {
+    $pinned = removePinnedMessage($_POST['removePin']);
+    header('Location: '. $url);
+}
+
+$pinnedMessages = showPinnedMessage($_GET['room']);
+
+
+//var_dump($pinnedMessages);
+
 require_once dirname(__DIR__) . '/views/room.phtml';
--- Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/04 - Projet Distortion/Exercice/models/Messages.php
+++ Modules/02-Developpement/PHP SQL/Module 02 - MVC, Classes et Cookies/01 - MVC (Modèle View Controller)/04 - Projet Distortion/Exercice/models/Messages.php
@@ -9,7 +9,7 @@ function showMessages($roomId): array
 {
     $connect =  getPDO();
     $request = $connect->prepare(
-        "SELECT * FROM messages WHERE room_id = :roomId" 
+        "SELECT * FROM messages WHERE room_id = :roomId"
     );
 
     $request->execute(
@@ -25,7 +25,7 @@ function addMessage($roomId, $msg)
     date_default_timezone_set('Europe/Paris');
     $connect =  getPDO();
     $request = $connect->prepare(
-        "INSERT INTO messages (`room_id`,`content`, `created_at`) VALUES (:roomId, :msg, CURRENT_TIMESTAMP  )"
+        "INSERT INTO messages (`room_id`,`content`, `created_at`) VALUES (:roomId, :msg, CURRENT_TIMESTAMP)"
     );
 
     $request->execute(
@@ -35,3 +35,49 @@ function addMessage($roomId, $msg)
         ]
     );
 }
+
+function removePinnedMessage($messageId)
+{
+    $connect =  getPDO();
+    $request = $connect->prepare(
+        "UPDATE messages
+        SET pinned = 0
+        WHERE id = :messageId"
+    );
+    $request->execute(
+        [
+            ':messageId' => $messageId
+        ]
+    );
+}
+
+function addPinnedMessage($messageId)
+{
+    $connect =  getPDO();
+    $request = $connect->prepare(
+        "UPDATE messages
+        SET pinned = 1
+        WHERE id = :messageId"  // Preciser la room et precicer l'id du messages
+    );
+    $request->execute(
+        [
+            ':messageId' => $messageId
+        ]
+    );
+}
+
+function showPinnedMessage($roomId)
+{
+    $connect =  getPDO();
+    $request = $connect->prepare(
+        "SELECT * FROM messages WHERE room_id = :roomId AND pinned = 1"
+    );
+
+    $request->execute(
+        [
+            ':roomId' => $roomId
+        ]
+    );
+    return $request->fetchAll();
+}
+
